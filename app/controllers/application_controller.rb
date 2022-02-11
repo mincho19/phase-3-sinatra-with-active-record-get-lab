e65661f1cabba713f1d25b2bada6d7be017ed7fc
Min Cho <minsangcho19@gmail.com>
2022-02-11 13:29:51 -0800

    commit tree fbf82580ad6a18a3d093dff72c6ed2fd42ecee84

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,28 @@
 class ApplicationController < Sinatra::Base
 
-  # add routes
+  set :default_content_type, 'application/json'
+
+  get '/bakeries' do
+    # get all the bakeries from the database
+    bakeries = Bakery.all
+    # send them back as a JSON array
+    bakeries.to_json
+  end
+
+  get '/bakeries/:id' do
+    bakery = Bakery.find(params[:id])
+    bakery.to_json(include: :baked_goods)
+
+  end
+
+  # get '/baked_goods/by_price' do
+  #   goods = BakedGood.order(name: :desc)
+  #   goods.to_json
+  # end
+
+  get '/baked_goods/most_expensive' do
+    price = BakedGood.order(price: :desc).limit(1)
+    price.to_json
+  end
 
 end
